0f0fd5d705d5748cc07bcf52e943d3871d575bde
vasilij korobov <kavyabass51@gmail.com>
2024-10-10 00:23:42 +0700

    вроде всё

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from product.apps import ProductConfig
-from product.views import products_list
+from product.views import products_list, products_details
 
 app_name = ProductConfig.name
 
 urlpatterns = [
-    path('', products_list)
+    path('', products_list, name='products_list'),
+    path('product/<int:pk>/', products_details, name='products_details')
     # path('admin/', admin.site.urls),
     # path('', include('product.urls', namespace='product'))
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from product.models import Product
 
@@ -11,4 +11,10 @@ from product.models import Product
 def products_list(request):
     products = Product.objects.all()
     context = {'products': products}
-    return render(request, 'product/base.html', context)
+    return render(request, 'product/products_list.html', context)
+
+
+def products_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product/products_details.html', context)
