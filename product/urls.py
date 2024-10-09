36f688dc08eb26946948f6b5d3b7d608ea6113ee
vasilij korobov <kavyabass51@gmail.com>
2024-10-09 19:39:34 +0700

    changes

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from product.apps import ProductConfig
-from product.views import index
+from product.views import products_list
 
 app_name = ProductConfig.name
 
 urlpatterns = [
-    path('', index)
+    path('', products_list)
     # path('admin/', admin.site.urls),
     # path('', include('product.urls', namespace='product'))
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from product.models import Product
+
 
 # Create your views here.
-def index(request):
-    return render(request, 'product/base.html')
+
+# def index(request):
+#    return render(request, 'product/base.html')
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'product/base.html', context)
