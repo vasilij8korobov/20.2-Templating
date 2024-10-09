e23f8863de161648e1696f2385b1135f4a651d41
vasilij korobov <kavyabass51@gmail.com>
2024-10-09 18:05:51 +0700

    целый список файлов был создан: base.html bootstrap.bundle.min.js bootstrap.min.css color-modes.js не раб.
    изминения:
    settings.py config
    urls.py product
    views.py product

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from product.apps import ProductConfig
+from product.views import index
 
 app_name = ProductConfig.name
 
 urlpatterns = [
+    path('', index)
     # path('admin/', admin.site.urls),
     # path('', include('product.urls', namespace='product'))
 ]
